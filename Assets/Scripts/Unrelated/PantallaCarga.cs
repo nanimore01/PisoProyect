8cde0db988672d46f4897087e662cd243e3a0da1
nanimore01 <nanimore01@gmail.com>
2025-12-18 15:56:41 -0300

    Ultimos cambios antes de entregar final

---
+++ Assets/Scripts/Unrelated/PantallaCarga.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PantallaCarga : MonoBehaviour
+{
+    public Image image;
+    public Animator animator;
+    
+    void Start()
+    {
+        //image.gameObject.SetActive(false);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
